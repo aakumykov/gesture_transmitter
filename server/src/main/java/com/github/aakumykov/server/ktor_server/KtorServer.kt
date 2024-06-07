7918aa337521e6d5bb88d4bd082b1793c97d7af1
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-07 21:43:00 +0300

    Добавляет предварительную версию класса KtorServer.

---
+++ server/src/main/java/com/github/aakumykov/server/ktor_server/KtorServer.kt
@@ -0,0 +1,86 @@
+package com.github.aakumykov.server.ktor_server
+
+import androidx.lifecycle.LifecycleOwner
+import com.github.aakumykov.server.GestureRecorder
+import com.google.gson.Gson
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.jetty.Jetty
+import io.ktor.server.jetty.JettyApplicationEngine
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.webSocket
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class KtorServer(
+    private val lifecycleOwner: LifecycleOwner,
+    private val coroutineScope: CoroutineScope,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
+
+    private var runningServer: JettyApplicationEngine? = null
+
+    fun run(address: String = DEFAULT_SERVER_ADDRESS, port: Int = DEFAULT_SERVER_PORT) {
+
+        coroutineScope.launch(coroutineDispatcher) {
+
+            runningServer = embeddedServer(
+                Jetty,
+                host = address,
+                port = port,
+            ) {
+                configureWebsockets(
+                    coroutineScope = coroutineScope,
+                    coroutineDispatcher = coroutineDispatcher,
+                    lifecycleOwner = lifecycleOwner,
+                    gestureRecorder = GestureRecorder,
+                    gson = Gson()
+                )
+            }.start(wait = true) // TODO: попробовать wait = false
+
+        }
+    }
+}
+
+fun Application.configureWebsockets(
+    coroutineScope: CoroutineScope,
+    coroutineDispatcher: CoroutineDispatcher,
+    lifecycleOwner: LifecycleOwner,
+    gestureRecorder: GestureRecorder,
+    gson: Gson
+) {
+    install(WebSockets)
+
+    routing {
+        gesturesRoute(
+            coroutineScope = coroutineScope,
+            coroutineDispatcher = coroutineDispatcher,
+            gestureRecorder = gestureRecorder,
+            lifecycleOwner = lifecycleOwner,
+            gson = Gson()
+        )
+    }
+}
+
+fun Routing.gesturesRoute(lifecycleOwner: LifecycleOwner,
+                          coroutineScope: CoroutineScope,
+                          coroutineDispatcher: CoroutineDispatcher,
+                          gestureRecorder: GestureRecorder,
+                          gson: Gson) {
+
+    webSocket {
+        gestureRecorder.recordedGesture.observe(lifecycleOwner) { gesture ->
+            val gestureJson = gson.toJson(gesture)
+            val textFrame = io.ktor.websocket.Frame.Text(gestureJson)
+            coroutineScope.launch(coroutineDispatcher) {
+                outgoing.send(textFrame)
+            }
+        }
+    }
+}
