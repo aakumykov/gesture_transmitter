68cdfe9c2dd6a3af0d0d8cffb546a5692839b8e8
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-08 22:16:58 +0300

    Добавляет пробный код в GesturePlayingService.

--- client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
+++ client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
@@ -12,10 +12,29 @@ class GesturePlayingService : AccessibilityService() {
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        /*event?.also { e ->
+            when(e.eventType) {
+                AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> debugWindowEvent(e)
+                AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> debugWindowContentEvent(e)
+                else -> {}
+            }
+        }*/
+        rootInActiveWindow?.also { rootView ->
+            debugLog("--------------")
+            debugLog("rootView.packageName: ${rootView.packageName}, childCount: ${rootView.childCount}")
+//            showChildrenRecursively(0, rootView.getChildren())
+        }
+    }
 
+    private fun debugLog(text: String) {
+        TODO("Not yet implemented")
     }
 
     override fun onInterrupt() {
 
+    }
+
+    companion object {
+        val TAG: String = GesturePlayingService::class.java.simpleName
     }
 }
