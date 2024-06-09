c8bac54a7885c66d3551e99ce30c1089331d3292
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-09 10:53:12 +0300

    Добавляет открытие настроек доступности из уведомления.

--- client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
+++ client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
@@ -2,27 +2,72 @@ package com.github.aakumykov.client
 
 import android.accessibilityservice.AccessibilityService
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.Intent
+import android.provider.Settings
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.github.aakumykov.client.extensions.openAccessibilitySettings
 import com.github.aakumykov.client.utils.NotificationChannelHelper
 import com.github.aakumykov.common.dateTimeString
 
 class GesturePlayingService : AccessibilityService() {
 
+    // TODO: убрать by lazy для ускорения работы
+
+    /*private val pendingContentIntent: PendingIntent by lazy {
+        PendingIntent.getActivity(
+            this,
+            CODE_ACTION_STOP,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_IMMUTABLE
+        )
+    }*/
+
+    private val stopServiceIntent: Intent by lazy {
+        Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+    }
+
+    private val stopServicePendingIntent: PendingIntent by lazy {
+        PendingIntent.getActivity(
+            applicationContext,
+            CODE_ACTION_STOP,
+            stopServiceIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+
+    private val stopServiceAction: NotificationCompat.Action by lazy {
+        NotificationCompat.Action(
+            R.drawable.ic_gesture_playing_service_stop,
+            getString(R.string.gesture_playing_service_action_stop),
+            stopServicePendingIntent
+        )
+    }
+
     private lateinit var notificationBuilder: NotificationCompat.Builder
 
     override fun onCreate() {
         super.onCreate()
+        debugStartStop("onCreate()")
         prepareNotificationChannel()
-        prepareNotificationBuilder()
+        prepareNotification()
         showDutyNotification()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.action?.also {
+            if (ACTION_STOP == it)
+                openAccessibilitySettings()
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        debugStartStop("onDestroy()")
         hideDutyNotification()
     }
 
@@ -34,10 +79,12 @@ class GesturePlayingService : AccessibilityService() {
         stopForeground(STOP_FOREGROUND_REMOVE)
     }
 
-    private fun prepareNotificationBuilder() {
+    private fun prepareNotification() {
         notificationBuilder = NotificationCompat.Builder(this, notificationChannelId)
             .setSmallIcon(R.drawable.ic_gesture_playing_service)
             .setPriority(NOTIFICATION_PRIORITY)
+//            .setContentIntent(pendingContentIntent)
+            .addAction(stopServiceAction)
     }
 
     private val dutyNotification: Notification by lazy {
@@ -86,9 +133,8 @@ class GesturePlayingService : AccessibilityService() {
 //        }
     }
 
-    private fun debugLog(text: String) {
-        Log.d(TAG, text)
-    }
+    private fun debugLog(text: String) { Log.d(TAG, text) }
+    private fun debugStartStop(text: String) { Log.d(TAG_START_STOP, text) }
 
     override fun onInterrupt() {
 
@@ -99,12 +145,16 @@ class GesturePlayingService : AccessibilityService() {
 
     companion object {
         val TAG: String = GesturePlayingService::class.java.simpleName
+        const val TAG_START_STOP: String = "START_STOP"
 
         const val CHROME_PACKAGE_NAME = "com.android.chrome"
 
         // TODO: вынести это в файл настроек?
         const val NOTIFICATION_PRIORITY = NotificationCompat.PRIORITY_LOW
         const val NOTIFICATION_CHANNEL_IMPORTANCE = NotificationManagerCompat.IMPORTANCE_LOW
+
+        const val CODE_ACTION_STOP: Int = 10
+        const val ACTION_STOP = "ACTION_STOP"
     }
 }
 
