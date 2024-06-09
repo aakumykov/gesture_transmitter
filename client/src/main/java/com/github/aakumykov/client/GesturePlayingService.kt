28a9486cdecff79f34db2c4ca0d172a4bf038f9a
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-09 10:19:39 +0300

    Добавляет уведомление о работающей службе повторения жестов.

--- client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
+++ client/src/main/java/com/github/aakumykov/client/GesturePlayingService.kt
@@ -1,16 +1,60 @@
 package com.github.aakumykov.client
 
 import android.accessibilityservice.AccessibilityService
+import android.app.Notification
+import android.content.Intent
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.github.aakumykov.client.utils.NotificationChannelHelper
 import com.github.aakumykov.common.dateTimeString
 
 class GesturePlayingService : AccessibilityService() {
 
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+
     override fun onCreate() {
         super.onCreate()
+        prepareNotificationChannel()
+        prepareNotificationBuilder()
+        showDutyNotification()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        hideDutyNotification()
+    }
 
+    private fun showDutyNotification() {
+        startForeground(notificationId, dutyNotification)
+    }
+
+    private fun hideDutyNotification() {
+        stopForeground(STOP_FOREGROUND_REMOVE)
+    }
+
+    private fun prepareNotificationBuilder() {
+        notificationBuilder = NotificationCompat.Builder(this, notificationChannelId)
+            .setSmallIcon(R.drawable.ic_gesture_playing_service)
+            .setPriority(NOTIFICATION_PRIORITY)
+    }
+
+    private val dutyNotification: Notification by lazy {
+        notificationBuilder
+            .setContentTitle(getString(R.string.gesture_playing_service_duty_notification_title))
+            .setContentText(getString(R.string.gesture_playing_service_duty_notification_text))
+            .build()
+    }
+
+    private fun prepareNotificationChannel() {
+        NotificationChannelHelper(NotificationManagerCompat.from(this))
+            .createNotificationChannel(
+                id = notificationChannelId,
+                importance = NOTIFICATION_CHANNEL_IMPORTANCE,
+                getString(R.string.gesture_playing_service_notification_channel_name),
+                getString(R.string.gesture_playing_service_notification_channel_description),
+            )
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -50,9 +94,17 @@ class GesturePlayingService : AccessibilityService() {
 
     }
 
+    private val notificationId: Int = R.id.gesture_playing_service_nitification_id
+    private val notificationChannelId: String get() = "${packageName}_notifications"
+
     companion object {
         val TAG: String = GesturePlayingService::class.java.simpleName
+
         const val CHROME_PACKAGE_NAME = "com.android.chrome"
+
+        // TODO: вынести это в файл настроек?
+        const val NOTIFICATION_PRIORITY = NotificationCompat.PRIORITY_LOW
+        const val NOTIFICATION_CHANNEL_IMPORTANCE = NotificationManagerCompat.IMPORTANCE_LOW
     }
 }
 
