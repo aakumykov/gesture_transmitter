7ce406c8b5aca137c2752cbb48015df88a0de18f
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-10 22:31:02 +0300

    Перенёс подключение библиотеки Google Gson из модуля data_model в common; добавил в AppComponent модуль GsonModule.

--- common/src/main/java/com/github/aakumykov/common/di/AppComponent.kt
+++ common/src/main/java/com/github/aakumykov/common/di/AppComponent.kt
@@ -1,10 +1,12 @@
 package com.github.aakumykov.common.di
 
 import com.github.aakumykov.common.di.modules.ContextModule
+import com.github.aakumykov.common.di.modules.GsonModule
 import dagger.Component
 
 @Component(modules = [
     ContextModule::class,
+    GsonModule::class
 ])
 interface AppComponent {
 
