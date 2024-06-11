be88932ab4d55f716bbc9239ac1f4946abeb3145
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-11 13:15:07 +0300

    Навигация с главного экрана в Клиент и Сервер.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/ComposeMainActivity.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/ComposeMainActivity.kt
@@ -5,47 +5,89 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.github.aakumykov.app_compose.ui.theme.Gesture_transmitterTheme
+
+const val DESTINATION_WELCOME = "DESTINATION_WELCOME"
+const val DESTINATION_CLIENT = "DESTINATION_CLIENT"
+const val DESTINATION_SERVER = "DESTINATION_SERVER"
+
 
 class ComposeMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            Gesture_transmitterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = DESTINATION_WELCOME
+            ) {
+                composable(DESTINATION_WELCOME) {
+                    WelcomeScreen(
+                        { navController.navigate(DESTINATION_CLIENT) },
+                        { navController.navigate(DESTINATION_SERVER) }
                     )
+                }
+                composable(DESTINATION_CLIENT) {
+                    ClientScreen {
+                        navController.popBackStack()
+                    }
+                }
+                composable(DESTINATION_SERVER) {
+                    ServerScreen {
+                        navController.popBackStack()
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ClientScreen(onBackButtonClick: () -> Unit) {
+    Text(text = "Клиент")
+}
+
+@Composable
+fun ServerScreen(onBackButtonClick: () -> Unit) {
+    Text(text = "Сервер")
+}
+
+
+@Composable
+fun WelcomeScreen(
+    onClientButtonClicked: () -> Unit,
+    onServerButtonClicked: () -> Unit
+) {
     Column {
-        MainButton(textRes = R.string.button_client) {
+        MainButton(
+            textRes = R.string.button_client,
+            onClick = onClientButtonClicked
+        )
 
-        }
-        MainButton(textRes = R.string.button_server) {
-
-        }
+        MainButton(
+            textRes = R.string.button_server,
+            onClick = onServerButtonClicked
+        )
     }
 }
+
 
 @Composable
 fun MainButton(
@@ -55,8 +97,8 @@ fun MainButton(
     Button(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(all = 24.dp),
-        onClick = { /*TODO*/ }
+            .padding(all = 12.dp),
+        onClick = { onClick.invoke() }
     ) {
         Text(text = stringResource(id = textRes))
     }
@@ -66,6 +108,6 @@ fun MainButton(
 @Composable
 fun GreetingPreview() {
     Gesture_transmitterTheme {
-        Greeting("Android")
+        WelcomeScreen({},{})
     }
 }
---
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/MainScreenType.kt
@@ -0,0 +1,7 @@
+package com.github.aakumykov.app_compose
+
+enum class MainScreenType {
+    WELCOME,
+    CLIENT,
+    SERVER
+}
