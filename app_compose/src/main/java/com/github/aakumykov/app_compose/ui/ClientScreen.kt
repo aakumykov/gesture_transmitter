4ca17e3c4192ec0766fa61b4d9a8385f922d2656
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-11 15:42:31 +0300

    Подключил lifecycle.runtime.compose, реагирую в Composable-функции на onResume.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/ui/ClientScreen.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/ui/ClientScreen.kt
@@ -4,15 +4,19 @@ import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.Lifecycle
 import com.github.aakumykov.app_compose.ui.components.SimpleButton
 import com.github.aakumykov.client.client_state_provider.ClientState
 import com.github.aakumykov.client.extensions.isAccessibilityServiceEnabled
@@ -33,10 +37,22 @@ fun ClientScreen(
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
+
+    val accButtonText = remember { mutableStateOf(context.isAccessibilityServiceEnabled()) }
+
+    LaunchedEffect(lifecycleState) {
+        when (lifecycleState) {
+            Lifecycle.State.RESUMED -> accButtonText.value = context.isAccessibilityServiceEnabled()
+            else -> {}
+        }
+    }
+
     Column {
 
         SimpleButton(
-            text = accServiceStateString(context),
+            text = accButtonTextReal(accButtonText),
             bgColor = colorResource(com.github.aakumykov.client.R.color.button_acc_service),
         ) {
             context.openAccessibilitySettings()
@@ -83,12 +99,22 @@ fun ClientScreen(
     }
 }
 
+@Composable
+fun accButtonTextReal(accButtonText: MutableState<Boolean>): String {
+    return if (accButtonText.value) stringResource(com.github.aakumykov.client.R.string.button_acc_service_enabled)
+    else stringResource(com.github.aakumykov.client.R.string.button_acc_service_disabled)
+}
+/*
+fun updateAccessibilityServiceButton(context: Context) {
+
+}
+
 fun accServiceStateString(context: Context): String {
     return context.getString(
         if (context.isAccessibilityServiceEnabled()) com.github.aakumykov.client.R.string.button_acc_service_disabled
         else com.github.aakumykov.client.R.string.button_acc_service_enabled
     )
-}
+}*/
 
 fun processClientState(clientState: ClientState) {
 
