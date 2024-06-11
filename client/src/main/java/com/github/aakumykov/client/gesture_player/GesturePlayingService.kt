fd806b8e457ca9c34b31c473630b93956f13ef34
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-11 17:36:19 +0300

    Удалил неиспользуемый код из GesturePlayingService.

--- client/src/main/java/com/github/aakumykov/client/gesture_player/GesturePlayingService.kt
+++ client/src/main/java/com/github/aakumykov/client/gesture_player/GesturePlayingService.kt
@@ -18,14 +18,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 class GesturePlayingService : AccessibilityService() {
 
-    // TODO: добавить ChromeDetector
-
-    private val trackedWindowHasAppeared: AtomicBoolean = AtomicBoolean(false)
-    private val trackedWindowNotVisible: Boolean get() = !trackedWindowHasAppeared.get()
-
     private var chromeIsLaunched: Boolean = false
-    private var currentWindowHasContent: Boolean = false
-
 
     private val gesturePlayer: GesturePlayer by lazy {
         GesturePlayer(this)
@@ -72,20 +65,6 @@ class GesturePlayingService : AccessibilityService() {
     }
 
 
-    // Место для динамической настройки AccessibilityService
-    override fun onServiceConnected() {
-        super.onServiceConnected()
-        /*getServiceInfo().apply {
-            eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED //or AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED
-            packageNames = arrayOf("com.android.chrome")
-            // flags = AccessibilityServiceInfo.DEFAULT;
-            notificationTimeout = 100
-        }.also {
-            setServiceInfo(it)
-        }*/
-    }
-
-
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         if (null != event) {
 
@@ -100,31 +79,7 @@ class GesturePlayingService : AccessibilityService() {
                     reportServerChromeIsActive(chromeIsLaunched)
                 }
             }
-
-//            chromeIsLaunched = currentWindowIsChromeWindow()
-//            currentWindowHasContent = currentWindowHasChild()
-
-            /*debugLog(TAG,
-                "Хром "
-                    + (if(chromeIsLaunched) "" else "не")
-                    + " запущен. "
-                    + "Текущее окно "
-                    + (if (currentWindowHasContent) "имеет содержимое" else "без содержимого")
-                    + "."
-            )*/
         }
-
-        /*if (null != event && event.isWindowStateChanged()) {
-            if (isAppWindow(GOOGLE_CHROME_PACKAGE_NAME, true)*//* && trackedWindowNotVisible*//*) {
-                debugLog("Гоголь Хром детектед, $dateTimeString")
-                trackedWindowHasAppeared.set(true)
-                connectToServer()
-            } else {
-                debugLog("Гоголь Хром ушёл в фон, $dateTimeString")
-                trackedWindowHasAppeared.set(false)
-                disconnectFromServer()
-            }
-        }*/
     }
 
     private fun reportServerChromeIsActive(isActive: Boolean) {
@@ -139,12 +94,7 @@ class GesturePlayingService : AccessibilityService() {
     }
 
 
-
     private fun debugLog(text: String) { Log.d(TAG, text) }
-    private fun debugLog(tag: String, text: String) { Log.d(tag, text) }
-    private fun errorLog(text: String) { Log.e(TAG, text) }
-    private fun errorLog(throwable: Throwable) { Log.e(TAG, ExceptionUtils.getErrorMessage(throwable), throwable) }
-    private fun errorLog(text: String, throwable: Throwable) { Log.e(TAG, text, throwable) }
 
 
     companion object {
@@ -152,21 +102,10 @@ class GesturePlayingService : AccessibilityService() {
     }
 }
 
-fun AccessibilityEvent.isWindowStateChanged(): Boolean {
-    return AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED == eventType
-}
-
 
 fun AccessibilityService.isAppWindow(checkedPackageName: String): Boolean {
     return when(rootInActiveWindow) {
         null -> false
         else -> rootInActiveWindow.packageName == checkedPackageName
-    }
-}
-
-fun AccessibilityService.currentWindowHasChild(): Boolean {
-    return when(rootInActiveWindow) {
-        null -> false
-        else -> rootInActiveWindow.childCount > 0
     }
 }
