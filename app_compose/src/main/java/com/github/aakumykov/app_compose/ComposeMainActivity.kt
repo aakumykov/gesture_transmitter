8314943e3cb9510316371fb1ce0b3efca68c6ba5
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-11 15:10:37 +0300

    Монструозное ручное внедрение зависимостей.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/ComposeMainActivity.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/ComposeMainActivity.kt
@@ -1,7 +1,6 @@
 package com.github.aakumykov.app_compose
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -19,7 +17,10 @@ import androidx.navigation.compose.rememberNavController
 import com.github.aakumykov.app_compose.ui.ClientScreen
 import com.github.aakumykov.app_compose.ui.ServerScreen
 import com.github.aakumykov.app_compose.ui.WelcomeScreen
+import com.github.aakumykov.client.client_state_provider.KtorStateProvider
+import com.github.aakumykov.client.gesture_client.GestureClient
 import com.github.aakumykov.common.settings_provider.SettingsProvider
+import com.github.aakumykov.data_model.utils.TimestampSupplier
 import com.github.aakumykov.prefs_module.PreferencesScreen
 import com.github.aakumykov.server.GestureRecorder
 import com.github.aakumykov.server.gesture_logger.RoomGestureLogger
@@ -28,7 +29,6 @@ import com.github.aakumykov.server.logDatabase
 import com.github.aakumykov.server.log_database.LoggingRepository
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
-import java.util.Date
 
 const val DESTINATION_WELCOME = "DESTINATION_WELCOME"
 const val DESTINATION_CLIENT = "DESTINATION_CLIENT"
@@ -38,24 +38,44 @@ const val DESTINATION_SETTINGS = "DESTINATION_SETTINGS"
 
 class ComposeMainActivity : ComponentActivity() {
 
+    //
+    // Монструозное ручное внедрение зависимостей, так как перейти на Dagger2 я не успел.
+    // Демонстрация владения технологией здесь: https://github.com/aakumykov/cloud_sync/tree/master/app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/di
+    //
+
     private val gestureRecorder by lazy { GestureRecorder }
 
-    private val gestureServer: GestureServer by lazy {
-        GestureServer(
-            Gson(),
-            RoomGestureLogger(
-                LoggingRepository(
-                    logDatabase.getLogMessageDAO(),
-                    Dispatchers.IO
-                )
-            ),
-            { Date().time }
-        )
+    private val gson: Gson by lazy { Gson() }
+
+    private val timestampSupplier by lazy { TimestampSupplier }
+
+    private val loggingMessageDAO by lazy { logDatabase.getLogMessageDAO() }
+
+    private val ioDispatcher by lazy { Dispatchers.IO }
+
+
+    private val loggingRepository by lazy {
+        LoggingRepository(loggingMessageDAO, ioDispatcher)
+    }
+
+    private val roomGestureLogger by lazy {
+        RoomGestureLogger(loggingRepository)
     }
 
     private val settingsProvider: SettingsProvider by lazy {
         SettingsProvider.getInstance(App.appContext)
     }
+
+    private val ktorStateProvider by lazy { KtorStateProvider }
+
+    private val gestureServer: GestureServer by lazy {
+        GestureServer(gson, roomGestureLogger, timestampSupplier)
+    }
+
+    private val gestureClient: GestureClient by lazy {
+        GestureClient.getInstance(gson, ktorStateProvider)
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,6 +97,9 @@ class ComposeMainActivity : ComponentActivity() {
                     navController = navController,
                     startDestination = DESTINATION_WELCOME
                 ) {
+                    //
+                    // Начальный экран
+                    //
                     composable(DESTINATION_WELCOME) {
                         WelcomeScreen(
                             onClientButtonClicked = { navController.navigate(DESTINATION_CLIENT) },
@@ -84,11 +107,20 @@ class ComposeMainActivity : ComponentActivity() {
                             onSettingsButtonClicked = { navigateToSettings(navController) },
                         )
                     }
+
+                    //
+                    // Клиент
+                    //
                     composable(DESTINATION_CLIENT) {
                         ClientScreen(
+                            gestureClient = gestureClient,
                             onSettingsButtonClicked = { navigateToSettings(navController) }
                         )
                     }
+
+                    //
+                    // Сервер
+                    //
                     composable(DESTINATION_SERVER) {
                         ServerScreen(
                             onSettingsButtonClicked = { navigateToSettings(navController) },
@@ -97,6 +129,10 @@ class ComposeMainActivity : ComponentActivity() {
                             gestureRecorder = gestureRecorder
                         )
                     }
+
+                    //
+                    // Настройки
+                    //
                     composable(DESTINATION_SETTINGS) {
                         PreferencesScreen(
                             settingsProvider = SettingsProvider.getInstance(App.appContext),
@@ -108,11 +144,12 @@ class ComposeMainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun navigateToSettings(navController: NavController) {
+        navController.navigate(DESTINATION_SETTINGS)
+        navController.navigate(DESTINATION_SETTINGS)
+        navController.navigate(DESTINATION_SETTINGS)
+    }
 }
 
 
-fun navigateToSettings(navController: NavController) {
-    navController.navigate(DESTINATION_SETTINGS)
-    navController.navigate(DESTINATION_SETTINGS)
-    navController.navigate(DESTINATION_SETTINGS)
-}
