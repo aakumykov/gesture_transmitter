170a134ddf8a699688ef54c9721b6c9ea7952677
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-12 20:58:12 +0300

    Держать подкомпоненты в singleton-ах плохая идея.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/App.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/App.kt
@@ -3,6 +3,8 @@ package com.github.aakumykov.app_compose
 import android.app.Application
 import android.content.Context
 import android.widget.Toast
+import com.github.aakumykov.common.SettingsProviderApp
+import com.github.aakumykov.common.settings_provider.SettingsProviderComponent
 import com.github.aakumykov.server.ServerApp
 
 class App : Application() {
@@ -11,6 +13,8 @@ class App : Application() {
         super.onCreate()
         ServerApp.prepareLogDatabase(this)
         _appContext = this
+
+        SettingsProviderApp.init(this)
     }
 
     companion object {
---
+++ common/src/main/java/com/github/aakumykov/common/settings_provider/SettingsProviderComponent.kt
@@ -0,0 +1,10 @@
+package com.github.aakumykov.common.settings_provider
+
+import dagger.Component
+
+@Component(modules = [
+//    AppContextModule::class
+])
+interface SettingsProviderComponent {
+
+}
