8a6678b453990bdd5f9a5eca9bbd8f2674bdf908
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-12 21:01:55 +0300

    Revert "Держать подкомпоненты в singleton-ах плохая идея."
    This reverts commit 170a134ddf8a699688ef54c9721b6c9ea7952677.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/App.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/App.kt
@@ -3,8 +3,6 @@ package com.github.aakumykov.app_compose
 import android.app.Application
 import android.content.Context
 import android.widget.Toast
-import com.github.aakumykov.common.SettingsProviderApp
-import com.github.aakumykov.common.settings_provider.SettingsProviderComponent
 import com.github.aakumykov.server.ServerApp
 
 class App : Application() {
@@ -13,8 +11,6 @@ class App : Application() {
         super.onCreate()
         ServerApp.prepareLogDatabase(this)
         _appContext = this
-
-        SettingsProviderApp.init(this)
     }
 
     companion object {
