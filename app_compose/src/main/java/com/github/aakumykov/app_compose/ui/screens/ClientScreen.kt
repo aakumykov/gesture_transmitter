3f74c1bddfef5aabe3fe92176406b554c01c8025
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-12 04:09:34 +0300

    Google Chrome должен запускаться после установке соединения с сервером.

--- app_compose/src/main/java/com/github/aakumykov/app_compose/ui/screens/ClientScreen.kt
+++ app_compose/src/main/java/com/github/aakumykov/app_compose/ui/screens/ClientScreen.kt
@@ -8,26 +8,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.Lifecycle
+import com.github.aakumykov.app_compose.funstions.app_launching.launchGoogleChrome
 import com.github.aakumykov.app_compose.funstions.gesture_client.connectToServer
 import com.github.aakumykov.app_compose.funstions.gesture_client.disconnectFromServer
-import com.github.aakumykov.app_compose.funstions.app_launching.launchGoogleChrome
-import com.github.aakumykov.app_compose.funstions.ui.pauseButtonText
 import com.github.aakumykov.app_compose.funstions.gesture_client.pauseResumeServerInteraction
+import com.github.aakumykov.app_compose.funstions.ui.pauseButtonText
 import com.github.aakumykov.app_compose.ui.gui_elements.client.ClientState
 import com.github.aakumykov.app_compose.ui.gui_elements.client.accessibilityButtonText
 import com.github.aakumykov.app_compose.ui.gui_elements.shared.SimpleButton
+import com.github.aakumykov.client.client_state_provider.ClientState
 import com.github.aakumykov.client.extensions.isAccessibilityServiceEnabled
 import com.github.aakumykov.client.extensions.openAccessibilitySettings
 import com.github.aakumykov.client.gesture_client.GestureClient
 import com.github.aakumykov.common.settings_provider.SettingsProvider
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.onEach
 
 @Composable
 fun ClientScreen(
@@ -43,7 +48,9 @@ fun ClientScreen(
     val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
 
     val accessibilityButtonText = remember { mutableStateOf(context.isAccessibilityServiceEnabled()) }
-    val clientState = remember { mutableStateOf(gestureClient.currentState) }
+    val clientCurrentState = remember { mutableStateOf(gestureClient.currentState) }
+
+    var chromeWasLaunchedOnConnected by rememberSaveable { mutableStateOf(false) }
 
     //
     // Наблюдаю за жизненным циклом Activity
@@ -56,10 +63,28 @@ fun ClientScreen(
     }
 
     //
-    // Наблюдаю за статусом Ktor-клиента ("GestureClient-а")
+    // Наблюдаю за статусом GestureClient-а и запускаю Google Chrome при первом
     //
     LaunchedEffect(lifecycleState) {
-        gestureClient.state.filterNotNull().collect { clientState.value = it }
+        gestureClient.state
+            .filterNotNull()
+            // Обновляю статус для интерфейса.
+            .onEach {
+                clientCurrentState.value = it
+            }
+            // Сбрасываю флаг запуска Хрома при отключении.
+            .onEach {
+                if (ClientState.CONNECTED != it)
+                    chromeWasLaunchedOnConnected = false
+            }
+            .filter { ClientState.CONNECTED == it }
+            // Запускаю Хром при подключении, если не был запущен.
+            .collect {
+                if (!chromeWasLaunchedOnConnected) {
+                    launchGoogleChrome(context)
+                    chromeWasLaunchedOnConnected = true
+                }
+            }
     }
 
 
@@ -90,7 +115,7 @@ fun ClientScreen(
         )
 
         SimpleButton(
-            text = stringResource(pauseButtonText(clientState)),
+            text = stringResource(pauseButtonText(clientCurrentState)),
             bgColor = colorResource(com.github.aakumykov.client.R.color.button_pause),
             onClick = {
                 pauseResumeServerInteraction(gestureClient, coroutineScope, coroutineDispatcher)
@@ -105,7 +130,7 @@ fun ClientScreen(
             }
         )
 
-        ClientState(clientState.value)
+        ClientState(clientCurrentState.value)
     }
 }
 
